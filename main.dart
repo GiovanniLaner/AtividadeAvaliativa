0732523f06631ff0037834e5dab9d58c75f642ae
Giovanni Laner <86385629+GiovanniLaner@users.noreply.github.com>
2021-07-14 22:23:07 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'layout1.dart';
+import 'layout2.dart';
+
+void main() {
+  runApp(MaterialApp(
+       //home: layout1(),
+       home: layout2(),
+  ));
+}
+
